9846a90769e7ef2552688233cab974ec267e059c
Power <sevnich@gmail.com>
2021-01-06 09:21:14 +0900

    identifier 내용 수정

--- 1. 변수/2. identifier.js
+++ 1. 변수/2. identifier.js
@@ -11,3 +11,12 @@ console.log(banana);
 console.log(BANANA);
 
 var my_name;
+
+// var my phone number;
+
+// 사용자 이름은 누구나 알아볼 수 있도록 구체적으로 지정하는 습관을 들이자
+// var myPhoneNumber;
+// var user_password; Python에서 많이 사용
+
+
+
