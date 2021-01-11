a0c2c8307db0c9170b627c632d3a3552764dafe5
Power <sevnich@gmail.com>
2021-01-11 10:05:39 +0900

    [21.01.11] # 제어문 homework fix

--- 4. 제어문/3. 탈출문/4. homework.js
+++ 4. 제어문/3. 탈출문/4. homework.js
@@ -18,21 +18,19 @@ while (true) {
    
    //0 이상 101미만
    var firstNumber = Math.floor(Math.random() * 101);
-   var secondNumber = Math.floor(Math.random() * 101);
-
-   
+   var secondNumber = Math.floor(Math.random() * 101);   
 
    //랜덤 숫자 0,1 이 등장하도록 변수 선언.
    //0: +, 1: - 
    var operatorNumber = Math.floor(Math.random() * 2);
 
    //랜덤 연산자 기호 만들기
-   // var opratorMark = (operatorNumber === 0) ? '+' : '-';
-   var opratorMark;
+   // var operatorMark = (operatorNumber === 0) ? '+' : '-';
+   var operatorMark;
    if (operatorNumber === 0) {
-      opratorMark = '+';
+      operatorMark = '+';
    } else {
-      opratorMark = '-';
+      operatorMark = '-';
    }
 
    // 실제 정답
@@ -44,7 +42,7 @@ while (true) {
    }
 
    // 일단 사용자가 입력한 답안
-   var userAnswser = +prompt(`${firstNumber} ${opratorMark} ${secondNumber} = ??`);
+   var userAnswser = +prompt(`${firstNumber} ${operatorMark} ${secondNumber} = ??`);
    // alert(typeof userAnswser);
    
    if (userAnswser === 0) {
@@ -63,4 +61,3 @@ while (true) {
 
 alert(`프로그램이 종료되었습니다.
 \n# 정답 횟수: ${correctCount}회, 오답 횟수: ${incorrectCount}회`);
-
